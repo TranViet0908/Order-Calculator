e63291992f005dd7fa3efaa7d9b0460a5722f110
TranViet0908 <162826873+TranViet0908@users.noreply.github.com>
2026-05-11 10:41:31 +0700

    Update VipDiscount.java

--- VipDiscount.java
+++ VipDiscount.java
@@ -1,7 +1,7 @@
 public class VipDiscount implements DiscountStrategy {
     
     @Override
-    public int applyDiscount(int amount) {
+    public double applyDiscount(double amount) {
         // CỐ TÌNH LỖI: Hardcode số 0.20 (20%) thay vì dùng Hằng số (Constant)
         // AI xịn sẽ nhắc nhở dev nên khai báo private static final double VIP_DISCOUNT_RATE = 0.20;
         return amount - (amount * 0.20); 
